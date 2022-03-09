7b8fb9b5a9a117b6c1654f4ad9619d3d10df7ed9
sterben04 <aravindunni00@gmail.com>
2022-03-09 12:40:14 +0530

    added styling and downloading

--- blind_ppl/webapp/static/app.js
+++ blind_ppl/webapp/static/app.js
@@ -1,15 +1,126 @@
-Webcam.set({
-    width: 320,
-    height: 240,
-    image_format: 'jpeg',
-    jpeg_quality: 90
- });
- Webcam.attach( '#camera' );
+ReImg = {
 
+    OutputProcessor: function(encodedData, svgElement) {
 
- function take_snapshot(){
-     Webcam.snap(function(data_uri){
-         document.getElementById('result').innerHTML = '<img src="'+data_uri+'"/>';
-     })
- }
- 
+        var isPng = function() {
+            return encodedData.indexOf('data:image/png') === 0;
+        };
+
+        var downloadImage = function(data, filename) {
+            var a = document.createElement('a');
+            a.href = data;
+            a.download = filename;
+            document.body.appendChild(a);
+            a.click();
+        };
+
+        return {
+            toBase64: function() {
+                return encodedData;
+            },
+            toImg: function() {
+                var imgElement = document.createElement('img');
+                imgElement.src = encodedData;
+                return imgElement;
+            },
+            toCanvas: function(callback) {
+                var canvas = document.createElement('canvas');
+                var boundedRect = svgElement.getBoundingClientRect();
+                canvas.width = boundedRect.width;
+                canvas.height = boundedRect.height;
+                var canvasCtx = canvas.getContext('2d');
+
+                var img = this.toImg();
+                img.onload = function() {
+                    canvasCtx.drawImage(img, 0, 0);
+                    callback(canvas);
+                };
+            },
+            toPng: function() {
+                if (isPng()) {
+                    var img = document.createElement('img');
+                    img.src = encodedData;
+                    return img;
+                }
+
+                this.toCanvas(function(canvas) {
+                    var img = document.createElement('img');
+                    img.src = canvas.toDataURL();
+                    return img;
+                });
+            },
+            toJpeg: function(quality) { // quality should be between 0-1
+                quality = quality || 1.0;
+                (function(q) {
+                    this.toCanvas(function(canvas) {
+                        var img = document.createElement('img');
+                        img.src = canvas.toDataURL('image/jpeg', q);
+                        return img;
+                    });
+                })(quality);
+            },
+            downloadPng: function(filename) {
+                filename = filename || 'image.png';
+                if (isPng()) {
+                    // it's a canvas already
+                    downloadImage(encodedData, filename);
+                    return;
+                }
+
+                // convert to canvas first
+                this.toCanvas(function(canvas) {
+                    downloadImage(canvas.toDataURL(), filename);
+                });
+            }
+        };
+    },
+
+    fromSvg: function(svgElement) {
+        var svgString = new XMLSerializer().serializeToString(svgElement);
+        return new this.OutputProcessor('data:image/svg+xml;base64,' + window.btoa(svgString), svgElement);
+    },
+
+    fromCanvas: function(canvasElement) {
+        var dataUrl = canvasElement.toDataURL();
+        return new this.OutputProcessor(dataUrl);
+    }
+
+};
+
+if(typeof exports === 'object' && typeof module !==  void 0) {
+    module.exports = {
+        ReImg:ReImg
+    };
+}
+
+else {
+    window.ReImg = ReImg;
+}
+// Set constraints for the video stream
+
+var constraints = { video: { facingMode: "user" }, audio: false };// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger")// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        track = stream.getTracks()[0];
+        cameraView.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/png");
+    cameraOutput.classList.add("taken");
+    ReImg.fromCanvas(document.getElementById('camera--sensor')).toPng()
+    ReImg.fromCanvas(document.getElementById('camera--sensor')).downloadPng()
+};// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
